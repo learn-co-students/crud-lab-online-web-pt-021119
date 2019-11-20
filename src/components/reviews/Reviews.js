64051ea97013019016b40aeacaf0d48f1602caf3
cyantis <cyantis@gmail.com>
2019-11-20 11:25:09 -0700

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -4,7 +4,7 @@ import Review from './Review';
 class Reviews extends Component {
 
 renderReviews = () => this.props.reviews.map(review => {
-  if(review.restaurantId === this.props.restaurant.id){
+  if(review.restaurantId === this.props.restaurantId){
     return <Review review={ review } deleteReview={ this.props.deleteReview }/>
     }
   }
