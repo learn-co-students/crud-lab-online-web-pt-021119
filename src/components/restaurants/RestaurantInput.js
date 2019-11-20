c797eacec782249c16a51ec87c433f9daaff1169
cyantis <cyantis@gmail.com>
2019-11-20 07:57:37 -0700

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -23,7 +23,6 @@ class RestaurantInput extends Component {
   render() {
     return (
       <div>
-        Restaurant Input
         <form onSubmit={ event => this.handleOnSubmit(event) }>
           <input type="text" onChange={event => this.handleOnChange(event)} value={this.state.text}></input>
           <input type="submit"></input>
--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
+
+renderRestaurants = () => this.props.restaurants.map(restaurant => <Restaurant restaurant={restaurant}/>)
+
   render() {
     return(
       <ul>
-        Restaurants Component
+        { this.renderRestaurants() }
       </ul>
     );
   }
